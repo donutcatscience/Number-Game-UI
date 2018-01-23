bc632220185eafdb3689e82c5575ab29bd21bfc8
Donutcat <31439092+donutcatscience@users.noreply.github.com>
2018-01-23 11:29:41 -0700

    One Hour Update
    -added multiple scenes
    -buttons to move between scenes
    -started number game main code

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    //load game scene
+    public void LoadLevel (string name)
+    {
+        Debug.Log("Level load requested for " +name);
+        Application.LoadLevel(name);
+    }
+
+    //handle quitting game
+    public void QuitRequest()
+    {
+        Debug.Log("I want to quit!");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MainGame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Welcome to Number Game!");
+        print("Pick a number in your head!");
+
+        int max = 1000;
+        int min = 1;
+
+        print("The highest number you can pick is " + max);
+        print("The lowest number you can pick is " + min);
+
+        print("Is the number higher or lower than 500?");
+        print("Up = higher, down = lower, return = equal.");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow)){
+            print("Up arrow pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("Down Arrow Pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("Return Key Pressed");
+        }
+    }
+}
